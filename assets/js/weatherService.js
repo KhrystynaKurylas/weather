038ee95a9699367f93372405eb9876aa92d5fbe4
KhrystynaKurylas <tincabest@gmail.com>
2017-05-15 17:40:15 +0300

    Update weatherService.js

--- assets/js/weatherService.js
+++ assets/js/weatherService.js
@@ -1,8 +1,4 @@
-
-function getWeatherData(lang, fnOK, fnError) {
-    navigator.geolocation.getCurrentPosition(locSuccess, locError);
-
-    function locSuccess(position) {
+ function locSuccess(position) {
         // Check cache
         var cache = localStorage.weatherCache && JSON.parse(localStorage.weatherCache);
         var currDate = new Date();
@@ -25,22 +21,4 @@ function getWeatherData(lang, fnOK, fnError) {
         }
     }
 
-    function locError(error) {
-        var message = 'Location error. ';
-        switch(error.code) {
-            case error.TIMEOUT:
-                message += 'A timeout occured! Please try again!';
-                break;
-            case error.POSITION_UNAVAILABLE:
-                message += 'We can\'t detect your location. Sorry!';
-                break;
-            case error.PERMISSION_DENIED:
-                message += 'Please allow geolocation access for this to work.';
-                break;
-            case error.UNKNOWN_ERROR:
-                message += 'An unknown error occured!';
-                break;
-        }
-        fnError.call(this, message);
-    }
-}
+    
