1e02a8f9c494fd6557797464b50fdf35875152ab
KhrystynaKurylas <tincabest@gmail.com>
2017-05-15 17:53:13 +0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,7 @@ $(function(){
 		scroller = $('#scroller'),
 		location = $('#location');
 
-	getWeatherData(locale, dataReceived,);
+	getWeatherData(locale, dataReceived,showError);
 
 	function dataReceived(data) {
 		// Get the offset from UTC (turn the offset minutes into ms)
