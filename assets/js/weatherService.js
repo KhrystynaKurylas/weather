a99f537d06b843c63ed4bab594243f3b065be2c0
KhrystynaKurylas <tincabest@gmail.com>
2017-05-16 12:26:00 +0300

    Update weatherService.js

--- assets/js/weatherService.js
+++ assets/js/weatherService.js
@@ -1,7 +1,7 @@
 function getWeatherByCity(lang, fnOK, fnError, cityName) {
     $.getJSON(
-        'http://api.openweathermap.org/data/2.5/forecast/daily?q=' 
-        + cityName + '&APPID=9649c92439ade00fa8117dec4613064b&cnt=16&units=metric' + '&lang=' + lang + '&callback=?',
+        'http://api.openweathermap.org/data/2.5/forecast?q=' 
+        + cityName + '&APPID=9649c92439ade00fa8117dec4613064b&units=metric' + '&lang=' + lang + '&callback=?',
         function (data) {
             fnOK.call(this, data);
         }
