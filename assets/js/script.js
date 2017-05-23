848ab59c973c1478deee3a8b01a9247e7c196433
KhrystynaKurylas <tincabest@gmail.com>
2017-05-23 12:57:30 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,41 @@
-$(".button-collapse").sideNav();
+      $(document).ready(function () {
+        var carousel = $("#carousel").waterwheelCarousel({
+         /*  flankingItems: 3,
+          movingToCenter: function ($item) {
+            $('#callback-output').prepend('movingToCenter: ' + $item.attr('id') + '<br/>');
+          },
+          movedToCenter: function ($item) {
+            $('#callback-output').prepend('movedToCenter: ' + $item.attr('id') + '<br/>');
+          },
+          movingFromCenter: function ($item) {
+            $('#callback-output').prepend('movingFromCenter: ' + $item.attr('id') + '<br/>');
+          },
+          movedFromCenter: function ($item) {
+            $('#callback-output').prepend('movedFromCenter: ' + $item.attr('id') + '<br/>');
+          },
+          clickedCenter: function ($item) {
+            $('#callback-output').prepend('clickedCenter: ' + $item.attr('id') + '<br/>');
+          } */
+        });
+
+       /*  $('#prev').bind('click', function () {
+          carousel.prev();
+          return false
+        });
+
+        $('#next').bind('click', function () {
+          carousel.next();
+          return false;
+        });
+
+        $('#reload').bind('click', function () {
+          newOptions = eval("(" + $('#newoptions').val() + ")");
+          carousel.reload(newOptions);
+          return false;
+        }); */
+
+      });
+
 $(function(){
 	var locale = 'ua',
 		weatherDiv = $('#weather'),
@@ -35,6 +72,7 @@ $(function(){
 			);
 		});
 		// Add the location to the page
+        document.getElementById('location').style.display = 'block';
 		location.html(city+', <b>'+country+'</b>');
 		weatherDiv.addClass('loaded');
 		// Set the slider to the first slide
